4b50bc8d81323e6dec1f2517f373fded83328e2c
Jaydev Khedkar <jaydevkhedkar257@gmail.com>
2026-05-21 04:23:08 +0530

    Time: 10 ms (26.4%), Space: 24.6 MB (26.53%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,9 +1,16 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        # 1. Count frequencies automatically
-        counts = Counter(nums)
+        count = {}
+        freq = [[] for i in range(len(nums) + 1)]
 
-        # 2. Grab the 'k' most common. 
-        # .most_common(k) returns pairs like [(num, count), (num, count)]
-        # We use a quick list comprehension to extract just the numbers.
-        return [item[0] for item in counts.most_common(k)]
+        for num in nums:
+            count[num] = 1 + count.get(num, 0)
+        for num, cnt in count.items():
+            freq[cnt].append(num)
+
+        res = []
+        for i in range(len(freq) - 1, 0, -1):
+            for num in freq[i]:
+                res.append(num)
+                if len(res) == k:
+                    return res
