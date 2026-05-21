2db57b62331b0bb2f6aea9dfd6e4ac17ec4a0620
Jaydev Khedkar <jaydevkhedkar257@gmail.com>
2026-05-22 02:09:45 +0530

    Time: 19 ms (77.01%), Space: 25.6 MB (54.82%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -1,35 +1,14 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        
-        if(len(nums) == 1): return nums[0]
+        res = [1] * (len(nums))
 
-        preprod = [1]*len(nums)
-        postprod = [1]*len(nums)
-        result = [1]*len(nums)
-        index = 0
-
-        for i in range(0, len(nums)):
-            if(i == 0):
-                preprod[i] = nums[i]
-            else:
-                preprod[i] = preprod[index]*nums[i]
-                index = i
-        
-        index = len(nums)-1
-
-        for i in range(len(nums)-1, -1, -1):
-            if(i == len(nums)-1):
-                postprod[i] = nums[i]
-            else:
-                postprod[i] = postprod[index]*nums[i]
-                index = i
-        
-        for i in range(0,len(nums)):
-            if(i == 0):
-                result[i] = postprod[i+1]
-            elif(i == (len(nums)-1)):
-                result[i] = preprod[i-1]
-            else:
-                result[i] = postprod[i+1]*preprod[i-1]
-        return result
+        prefix = 1
+        for i in range(len(nums)):
+            res[i] = prefix
+            prefix *= nums[i]
+        postfix = 1
+        for i in range(len(nums) - 1, -1, -1):
+            res[i] *= postfix
+            postfix *= nums[i]
+        return res
         
