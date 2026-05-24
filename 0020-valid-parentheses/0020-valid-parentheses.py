582a4028e8a7f875e066ac4dde06d8dae211d036
Jaydev Khedkar <jaydevkhedkar257@gmail.com>
2026-05-25 03:28:49 +0530

    Time: 0 ms (100%), Space: 19.3 MB (67.2%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -1,29 +1,29 @@
 class Solution:
     def isValid(self, s: str) -> bool:
-        if(len(s) == 1): return False
-        start = ["(","[","{"]
-        end = [")","]","}"]
+        if len(s) == 1:
+            return False
+        start = ["(", "[", "{"]
+        end = [")", "]", "}"]
         valid = dict(zip(start, end))
         stack = []
         ptr = -1
-        
-        for i in s:
-            if(ptr == -1 and i not in valid): return False
 
-            if(i in valid):
+        for i in s:
+            if ptr == -1 and i not in valid:
+                return False
+
+            if i in valid:
                 stack.append(valid[i])
                 ptr += 1
 
-            elif(stack[ptr] == i):
+            elif stack[ptr] == i:
                 stack.pop()
                 ptr -= 1
 
-            elif(stack[ptr] != i):
+            elif stack[ptr] != i:
                 return False
 
-        if(len(stack) != 0):
+        if len(stack) != 0:
             return False
 
         return True
-        
-
