178ad217284ad3a518aa4166040fd4921717e63f
Jaydev Khedkar <jaydevkhedkar257@gmail.com>
2026-05-28 07:17:24 +0530

    Time: 103 ms (45.64%), Space: 34.4 MB (23.4%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.py
+++ 0739-daily-temperatures/0739-daily-temperatures.py
@@ -1,12 +1,11 @@
 class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
-        answer = [0] * len(temperatures)
-        stack = []  # stores indices, decreasing temperatures
+        res = [0] * len(temperatures)
+        stack = []  # pair: [temp, index]
 
-        for i, temp in enumerate(temperatures):
-            while stack and temp > temperatures[stack[-1]]:
-                idx = stack.pop()
-                answer[idx] = i - idx
-            stack.append(i)
-
-        return answer  # unpopped indices stay 0 (no warmer day)
+        for i, t in enumerate(temperatures):
+            while stack and t > stack[-1][0]:
+                stackT, stackInd = stack.pop()
+                res[stackInd] = i - stackInd
+            stack.append((t, i))
+        return res
