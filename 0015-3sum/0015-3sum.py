4bd9e1489f5b778cffb0b0cd17a8244675f06ce1
Jaydev Khedkar <jaydevkhedkar257@gmail.com>
2026-06-02 09:02:24 +0530

    Time: 435 ms (97.59%), Space: 22.4 MB (32.02%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -1,24 +1,27 @@
 class Solution:
-    def threeSum(self, nums: list[int]) -> list[list[int]]:
-        snums = sorted(nums)
-        triplets = []
-        for i in range(len(snums) - 1):
-            if i > 0 and snums[i] == snums[i-1]:  # skip duplicate i
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        res = []
+        nums.sort()
+
+        for i, a in enumerate(nums):
+            if a > 0:
+                break
+
+            if i > 0 and a == nums[i - 1]:
                 continue
-            j = i + 1
-            k = len(snums) - 1
-            while j < k:
-                total = snums[i] + snums[j] + snums[k]
-                if total == 0:
-                    triplets.append([snums[i], snums[j], snums[k]])
-                    j += 1
-                    k -= 1
-                    while j < k and snums[j] == snums[j-1]:  # skip duplicate j
-                        j += 1
-                    while j < k and snums[k] == snums[k+1]:  # skip duplicate k
-                        k -= 1
-                elif total < 0:
-                    j += 1
+
+            l, r = i + 1, len(nums) - 1
+            while l < r:
+                threeSum = a + nums[l] + nums[r]
+                if threeSum > 0:
+                    r -= 1
+                elif threeSum < 0:
+                    l += 1
                 else:
-                    k -= 1
-        return triplets
+                    res.append([a, nums[l], nums[r]])
+                    l += 1
+                    r -= 1
+                    while nums[l] == nums[l - 1] and l < r:
+                        l += 1
+
+        return res
