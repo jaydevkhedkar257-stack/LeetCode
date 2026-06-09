d9dcc25c1b9865c06595c0fc2b7c0f203aebd8ef
Jaydev Khedkar <jaydevkhedkar257@gmail.com>
2026-06-09 07:40:30 +0530

    Time: 0 ms (100%), Space: 27.9 MB (21.99%) - LeetHub

--- 0143-reorder-list/0143-reorder-list.py
+++ 0143-reorder-list/0143-reorder-list.py
@@ -3,21 +3,25 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+
 class Solution:
     def reorderList(self, head: Optional[ListNode]) -> None:
-        if(head is None or head.next is None): return
-        stack = []
-        temp = head
-        while(temp):
-            stack.append(temp)
-            temp = temp.next
-        lenght = len(stack)
-        temp = head
-        for i in range(lenght//2):
-            itm = stack.pop()
-            itm.next = temp.next
-            temp.next = itm
-            temp = temp.next.next
-        temp.next = None
-         
-        
+        slow, fast = head, head.next
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+
+        second = slow.next
+        prev = slow.next = None
+        while second:
+            tmp = second.next
+            second.next = prev
+            prev = second
+            second = tmp
+
+        first, second = head, prev
+        while second:
+            tmp1, tmp2 = first.next, second.next
+            first.next = second
+            second.next = tmp1
+            first, second = tmp1, tmp2
