9389da35bd2c13af2d4333fe17e7a5f9a2322d14
Jaydev Khedkar <jaydevkhedkar257@gmail.com>
2026-06-14 23:27:10 +0530

    Time: 133 ms (40.73%), Space: 76.9 MB (61.1%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.py
+++ 0146-lru-cache/0146-lru-cache.py
@@ -1,49 +1,39 @@
 class Node:
-    def __init__(self, key=0, val=0):
-        self.key = key
-        self.val = val
-        self.prev = None
-        self.next = None
+    def __init__(self, key, val):
+        self.key, self.val = key, val
+        self.prev = self.next = None
 
 class LRUCache:
     def __init__(self, capacity: int):
         self.cap = capacity
-        self.map = {}  # key -> Node
+        self.cache = {}  # map key to node
 
-        # Sentinel head and tail (dummy nodes)
-        # Most Recently Used end = tail.prev
-        # Least Recently Used end = head.next
-        self.head = Node()
-        self.tail = Node()
-        self.head.next = self.tail
-        self.tail.prev = self.head
+        self.left, self.right = Node(0, 0), Node(0, 0)
+        self.left.next, self.right.prev = self.right, self.left
 
-    def _remove(self, node):
-        node.prev.next = node.next
-        node.next.prev = node.prev
+    def remove(self, node):
+        prev, nxt = node.prev, node.next
+        prev.next, nxt.prev = nxt, prev
 
-    def _insert_at_tail(self, node):
-        node.prev = self.tail.prev
-        node.next = self.tail
-        self.tail.prev.next = node
-        self.tail.prev = node
+    def insert(self, node):
+        prev, nxt = self.right.prev, self.right
+        prev.next = nxt.prev = node
+        node.next, node.prev = nxt, prev
 
     def get(self, key: int) -> int:
-        if key not in self.map:
-            return -1
-        node = self.map[key]
-        self._remove(node)
-        self._insert_at_tail(node)  # mark as recently used
-        return node.val
+        if key in self.cache:
+            self.remove(self.cache[key])
+            self.insert(self.cache[key])
+            return self.cache[key].val
+        return -1
 
     def put(self, key: int, value: int) -> None:
-        if key in self.map:
-            self._remove(self.map[key])
-        node = Node(key, value)
-        self.map[key] = node
-        self._insert_at_tail(node)
+        if key in self.cache:
+            self.remove(self.cache[key])
+        self.cache[key] = Node(key, value)
+        self.insert(self.cache[key])
 
-        if len(self.map) > self.cap:
-            lru = self.head.next      # LRU is just after dummy head
-            self._remove(lru)
-            del self.map[lru.key]
+        if len(self.cache) > self.cap:
+            lru = self.left.next
+            self.remove(lru)
+            del self.cache[lru.key]
