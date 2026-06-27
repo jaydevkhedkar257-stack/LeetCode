723be717fe536ee918ae65836a5eafa4d9483bca
Jaydev Khedkar <jaydevkhedkar257@gmail.com>
2026-06-27 20:40:44 +0530

    Time: 9 ms (63.8%), Space: 22.7 MB (81.31%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
@@ -1,36 +1,24 @@
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
 class Solution:
-    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
-        if not lists: return None
-        while(len(lists) > 1):
-            dummy = ListNode(0)
-            temp3 = dummy
-            temp1 = lists[0]
-            temp2 = lists[1]
-            while(temp1 and temp2):
-                if(temp1.val > temp2.val):
-                    temp3.next = temp2
-                    temp3 = temp3.next
-                    temp2 = temp2.next
-                else:
-                    temp3.next = temp1
-                    temp3 = temp3.next
-                    temp1 = temp1.next
-            if(temp1):
-                while(temp1):
-                    temp3.next = temp1
-                    temp1 = temp1.next
-                    temp3 = temp3.next
+    def mergeTwo(self, l1, l2):
+        dummy = ListNode(0)
+        tail = dummy
+        while l1 and l2:
+            if l1.val <= l2.val:
+                tail.next, l1 = l1, l1.next
             else:
-                while(temp2):
-                    temp3.next = temp2
-                    temp2 = temp2.next
-                    temp3 = temp3.next
-            lists.pop(0)
-            lists.pop(0)
-            lists.append(dummy.next)
+                tail.next, l2 = l2, l2.next
+            tail = tail.next
+        tail.next = l1 if l1 else l2
+        return dummy.next
+
+    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+        if not lists:
+            return None
+        while len(lists) > 1:
+            merged = []
+            for i in range(0, len(lists), 2):
+                l1 = lists[i]
+                l2 = lists[i + 1] if i + 1 < len(lists) else None
+                merged.append(self.mergeTwo(l1, l2))
+            lists = merged
         return lists[0]
