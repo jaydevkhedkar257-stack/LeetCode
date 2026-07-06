9ce1127c93e51a23d12dd082246df536f4f3ebed
Jaydev Khedkar <jaydevkhedkar257@gmail.com>
2026-07-06 21:16:29 +0530

    Time: 0 ms (100%), Space: 20.5 MB (39.41%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.py
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.py
@@ -1,20 +1,16 @@
 class Solution:
+    def dfs(self, root):
+        if root is None:
+            return 0
+        left_height = self.dfs(root.left)
+        if left_height == -1:
+            return -1
+        right_height = self.dfs(root.right)
+        if right_height == -1:
+            return -1
+        if abs(left_height - right_height) > 1:
+            return -1
+        return 1 + max(left_height, right_height)
+
     def isBalanced(self, root: Optional[TreeNode]) -> bool:
-        def check(node):
-            if not node:
-                return 0  # height of empty tree
-
-            left = check(node.left)
-            if left == -1:
-                return -1  # imbalance found below, propagate up
-
-            right = check(node.right)
-            if right == -1:
-                return -1
-
-            if abs(left - right) > 1:
-                return -1  # this node is unbalanced
-
-            return max(left, right) + 1  # normal height
-
-        return check(root) != -1
+        return self.dfs(root) != -1
