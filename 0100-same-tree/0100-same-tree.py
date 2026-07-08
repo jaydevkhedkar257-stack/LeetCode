9539ee771f109a8ea80788aa4ea8ef2464808340
Jaydev Khedkar <jaydevkhedkar257@gmail.com>
2026-07-08 18:34:11 +0530

    Time: 0 ms (100%), Space: 19.3 MB (30.38%) - LeetHub

--- 0100-same-tree/0100-same-tree.py
+++ 0100-same-tree/0100-same-tree.py
@@ -1,21 +1,9 @@
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
 class Solution:
-    def dfs(self, root, stack):
-        if(root is None):
-            stack.append(root)
-            return 
-        stack.append(root.val)
-        self.dfs(root.left, stack)
-        self.dfs(root.right, stack)
     def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
-        stack1 = []
-        stack2 = []
-        self.dfs(p, stack1)
-        self.dfs(q, stack2)
-        print(stack1, stack2)
-        return stack1 == stack2
+        if p is None and q is None:
+            return True
+        if p is None or q is None:
+            return False
+        return (p.val == q.val
+                and self.isSameTree(p.left, q.left)
+                and self.isSameTree(p.right, q.right))
