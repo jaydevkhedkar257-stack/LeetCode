60ab13c00e4473c61016ec6bf956b8f229fa53c2
Jaydev Khedkar <jaydevkhedkar257@gmail.com>
2026-07-10 03:17:36 +0530

    Time: 42 ms (47.23%), Space: 19.5 MB (61.92%) - LeetHub

--- 0572-subtree-of-another-tree/0572-subtree-of-another-tree.py
+++ 0572-subtree-of-another-tree/0572-subtree-of-another-tree.py
@@ -1,32 +1,14 @@
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
 class Solution:
-    def isSubroot(self, root, subroot, troots):
-        if(root is None):
-            return
-        if(root.val == subroot.val):
-            troots.append(root)
-        return self.isSubroot(root.left,subroot,troots) or self.isSubroot(root.right,subroot,troots)
-    
-    def isSameTree(self, troot, subRoot):
-        if(subRoot is None and troot is None):
-            return True
-        if(subRoot is None and troot is not None):
+    def isSubtree(self, root, subRoot):
+        if root is None:
             return False
-        if(subRoot is not None and troot is None):
-            return False 
-        # print(troot.val, subRoot.val)
-        return subRoot.val == troot.val and self.isSameTree(troot.left, subRoot.left) and self.isSameTree(troot.right, subRoot.right)
-        
-    def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
-        troots = []
-        self.isSubroot(root, subRoot, troots)
-        # print(troot.val)
-        res = False
-        for i in troots:
-            res = res or self.isSameTree(i, subRoot)
-        return res
+        if self.isSameTree(root, subRoot):
+            return True
+        return self.isSubtree(root.left, subRoot) or self.isSubtree(root.right, subRoot)
+
+    def isSameTree(self, a, b):
+        if a is None and b is None:
+            return True
+        if a is None or b is None:
+            return False
+        return a.val == b.val and self.isSameTree(a.left, b.left) and self.isSameTree(a.right, b.right)
