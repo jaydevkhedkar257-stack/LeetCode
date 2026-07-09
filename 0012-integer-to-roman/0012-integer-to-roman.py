460671f0d3241e4e1b8e19943e7a28ed40468daf
Jaydev Khedkar <jaydevkhedkar257@gmail.com>
2026-07-09 16:46:41 +0530

    Time: 3 ms (77.57%), Space: 19.3 MB (63.8%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.py
+++ 0012-integer-to-roman/0012-integer-to-roman.py
@@ -1,48 +1,11 @@
 class Solution:
-
     def intToRoman(self, num: int) -> str:
+        vals = [(1000,"M"),(900,"CM"),(500,"D"),(400,"CD"),(100,"C"),
+                 (90,"XC"),(50,"L"),(40,"XL"),(10,"X"),(9,"IX"),
+                 (5,"V"),(4,"IV"),(1,"I")]
         res = ""
-        while(num > 0):
-            print(num, res)
-            if(str(num)[0] == "9"):
-                if(num >= 900):
-                    num -= 900
-                    res += "CM"
-                elif(num >= 90):
-                    num -= 90
-                    res += "XC"
-                elif(num >= 9):
-                    num -= 9
-                    res += "IX"
-            elif(str(num)[0] == "4"):
-                if(num >= 400):
-                    num -= 400
-                    res += "CD"
-                elif(num >= 40):
-                    num -= 40
-                    res += "XL"
-                elif(num >= 4):
-                    num -= 4
-                    res += "IV"
-            elif(num >= 1000):
-                num -= 1000
-                res += "M"
-            elif(num >= 500):
-                num -= 500
-                res += "D"
-            elif(num >= 100):
-                num -= 100
-                res += "C"
-            elif(num >= 50):
-                num -= 50
-                res += "L"
-            elif(num >= 10):
-                num -= 10
-                res += "X"
-            elif(num >= 5):
-                num -= 5
-                res += "V"
-            else:
-                num -= 1
-                res += "I"
+        for v, sym in vals:
+            while num >= v:
+                res += sym
+                num -= v
         return res
