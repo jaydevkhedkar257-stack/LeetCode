e1ae8616212eb70cca252c54956c8a0d32409be4
Jaydev Khedkar <jaydevkhedkar257@gmail.com>
2026-07-11 16:23:03 +0530

    Time: 72 ms (17.72%), Space: 22.7 MB (43.85%) - LeetHub

--- 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.py
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.py
@@ -1,30 +1,18 @@
 # Definition for a binary tree node.
 # class TreeNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
 
 class Solution:
-    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-        # if(p is None and q is None):
-        #     return root
-        # if(p is None or q is None):
-        #     return p or q
-        if(p.val > q.val):
-            p, q = q, p
-        # def ischild(root, child):
-        #     if(root is None):
-        #         return False
-        #     return root.val == child.val or ischild(root.left, child) or ischild(root.right, child)
-        # if(ischild(p, q)):
-        #     return p
-        # elif(ischild(q, p)):
-        #     return q
-        curr = root
-        while(curr.val < p.val or curr.val > q.val):
-            if(curr.val < p.val):
-                curr = curr.right
+    def lowestCommonAncestor(self, root: TreeNode, p: TreeNode, q: TreeNode) -> TreeNode:
+        cur = root
+
+        while cur:
+            if p.val > cur.val and q.val > cur.val:
+                cur = cur.right
+            elif p.val < cur.val and q.val < cur.val:
+                cur = cur.left
             else:
-                curr = curr.left
-        return curr
+                return cur
