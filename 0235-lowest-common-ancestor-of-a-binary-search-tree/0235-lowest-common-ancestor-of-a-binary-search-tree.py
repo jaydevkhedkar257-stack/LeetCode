ddf355376c20e1244903e801891fc2b0e750335a
Jaydev Khedkar <jaydevkhedkar257@gmail.com>
2026-07-11 16:11:47 +0530

    Time: 64 ms (62.93%), Space: 22.7 MB (43.85%) - LeetHub

--- 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.py
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.py
@@ -7,20 +7,20 @@
 
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-        if(p is None and q is None):
-            return root
-        if(p is None or q is None):
-            return p or q
+        # if(p is None and q is None):
+        #     return root
+        # if(p is None or q is None):
+        #     return p or q
         if(p.val > q.val):
             p, q = q, p
-        def ischild(root, child):
-            if(root is None):
-                return False
-            return root.val == child.val or ischild(root.left, child) or ischild(root.right, child)
-        if(ischild(p, q)):
-            return p
-        elif(ischild(q, p)):
-            return q
+        # def ischild(root, child):
+        #     if(root is None):
+        #         return False
+        #     return root.val == child.val or ischild(root.left, child) or ischild(root.right, child)
+        # if(ischild(p, q)):
+        #     return p
+        # elif(ischild(q, p)):
+        #     return q
         curr = root
         while(curr.val < p.val or curr.val > q.val):
             if(curr.val < p.val):
