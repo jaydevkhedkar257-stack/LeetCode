5750dd4f3aa4c86504838824beb5f53102eaa1d9
Jaydev Khedkar <jaydevkhedkar257@gmail.com>
2026-08-03 00:26:13 +0530

    Time: 87 ms (66.37%), Space: 22.6 MB (60.4%) - LeetHub

--- 0297-serialize-and-deserialize-binary-tree/0297-serialize-and-deserialize-binary-tree.py
+++ 0297-serialize-and-deserialize-binary-tree/0297-serialize-and-deserialize-binary-tree.py
@@ -1,37 +1,40 @@
-from collections import deque
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
 
 class Codec:
 
-    def serialize(self, root):
-        if not root:
-            return "N"
-        q = deque([root])
+    # Encodes a tree to a single string.
+    def serialize(self, root: Optional[TreeNode]) -> str:
         res = []
-        while q:
-            node = q.popleft()
-            if node:
-                res.append(str(node.val))
-                q.append(node.left)
-                q.append(node.right)
-            else:
+
+        def dfs(node):
+            if not node:
                 res.append("N")
+                return
+            res.append(str(node.val))
+            dfs(node.left)
+            dfs(node.right)
+
+        dfs(root)
         return ",".join(res)
 
-    def deserialize(self, data):
+    # Decodes your encoded data to tree.
+    def deserialize(self, data: str) -> Optional[TreeNode]:
         vals = data.split(",")
-        if vals[0] == "N":
-            return None
-        root = TreeNode(int(vals[0]))
-        q = deque([root])
-        i = 1
-        while q:
-            node = q.popleft()
-            if vals[i] != "N":
-                node.left = TreeNode(int(vals[i]))
-                q.append(node.left)
-            i += 1
-            if vals[i] != "N":
-                node.right = TreeNode(int(vals[i]))
-                q.append(node.right)
-            i += 1
-        return root
+        self.i = 0
+
+        def dfs():
+            if vals[self.i] == "N":
+                self.i += 1
+                return None
+            node = TreeNode(int(vals[self.i]))
+            self.i += 1
+            node.left = dfs()
+            node.right = dfs()
+            return node
+
+        return dfs()
