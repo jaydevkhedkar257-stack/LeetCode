669b502c17d58e07b295aeee7d0cb9a9bd9e545b
Jaydev Khedkar <jaydevkhedkar257@gmail.com>
2026-09-02 20:32:41 +0530

    Time: 0 ms (100%), Space: 19.2 MB (84.34%) - LeetHub

---
+++ 3875-construct-uniform-parity-array-i/3875-construct-uniform-parity-array-i.py
@@ -0,0 +1,13 @@
+class Solution:
+    def uniformArray(self, nums1: list[int]) -> bool:
+        if sum(nums1) % 2 == 0 or len(nums1) == 1:
+            return True
+        i, j = 0, len(nums1) - 1
+        # while i <= j:
+        #     while nums1[i] % 2 == 0:
+        #         if nums1[j] % 2 != 0:
+        #             nums1[i] = nums1[i] - nums1[j]
+        #         j -= 1
+        #     j = len(nums1) - 1
+        #     i += 1
+        return True
