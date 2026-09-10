511f6f78a7ce8011a38b7aa7f482061ad6fa7dee
Jaydev Khedkar <jaydevkhedkar257@gmail.com>
2026-09-11 01:00:18 +0530

    Time: 9 ms (37.47%), Space: 21.1 MB (47.37%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,16 +1,9 @@
 class Solution:
-    def majorityElement(self, nums: List[int]) -> int:
-        check_set = defaultdict()
-        for i in nums:
-            if i in check_set:
-                check_set[i] += 1
-            else:
-                check_set[i] = 1
-        max = 0
-        max_element = 0
-        for i in check_set:
-            if check_set[i] >= max:
-                max_element = i
-                max = check_set[i]
-        return max_element
+    def majorityElement(self, nums):
+        res = count = 0
 
+        for num in nums:
+            if count == 0:
+                res = num
+            count += (1 if num == res else -1)
+        return res
