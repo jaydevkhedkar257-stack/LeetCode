a42b15d348342512fab9d1281f42617db3e72ae7
Jaydev Khedkar <jaydevkhedkar257@gmail.com>
2026-09-12 16:22:07 +0530

    Time: 62 ms (35.24%), Space: 25.2 MB (49.99%) - LeetHub

--- 0705-design-hashset/0705-design-hashset.py
+++ 0705-design-hashset/0705-design-hashset.py
@@ -1,23 +1,33 @@
+class ListNode:
+    def __init__(self, key: int):
+        self.key = key
+        self.next = None
+
 class MyHashSet:
 
     def __init__(self):
-        self.arr = []
+        self.set = [ListNode(0) for _ in range(10**4)]
 
     def add(self, key: int) -> None:
-        if key not in self.arr:
-            self.arr.append(key)
+        cur = self.set[key % len(self.set)]
+        while cur.next:
+            if cur.next.key == key:
+                return
+            cur = cur.next
+        cur.next = ListNode(key)
 
     def remove(self, key: int) -> None:
-        if key in self.arr:
-            self.arr.remove(key)
+        cur = self.set[key % len(self.set)]
+        while cur.next:
+            if cur.next.key == key:
+                cur.next = cur.next.next
+                return
+            cur = cur.next
 
     def contains(self, key: int) -> bool:
-        if key in self.arr:
-            return True
+        cur = self.set[key % len(self.set)]
+        while cur.next:
+            if cur.next.key == key:
+                return True
+            cur = cur.next
         return False
-
-# Your MyHashSet object will be instantiated and called as such:
-# obj = MyHashSet()
-# obj.add(key)
-# obj.remove(key)
-# param_3 = obj.contains(key)
