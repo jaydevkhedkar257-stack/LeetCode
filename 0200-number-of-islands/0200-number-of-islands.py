78f2b8006faecc70c107e22af5ba47b4557b18df
Jaydev Khedkar <jaydevkhedkar257@gmail.com>
2026-09-13 23:33:13 +0530

    Time: 290 ms (7.76%), Space: 34.6 MB (7.05%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,26 +1,25 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         hashset = set()
-        count = 0
+        res = []
         def backtrack(r, c, curr):
             if min(r, c) < 0 or r >= len(grid) or c >= len(grid[0]) or (r, c) in hashset:
-                return 
+                return []
             elif grid[r][c] == "1":
                 hashset.add((r,c))
-                # curr.append((r,c))
+                curr.append((r,c))
                 backtrack(r + 1, c, curr)
                 backtrack(r - 1, c, curr)
                 backtrack(r, c + 1, curr)
                 backtrack(r, c - 1, curr)
-            return 
+            return curr
         for i in range(len(grid)):
             for j in range(len(grid[0])):
                 if grid[i][j] == "0":
                     continue
                 if (i, j) in hashset:
                     continue
-                count += 1
-                backtrack(i, j, [])
-        return count
+                res.append(backtrack(i, j, []))
+        return len(res)
 
             
