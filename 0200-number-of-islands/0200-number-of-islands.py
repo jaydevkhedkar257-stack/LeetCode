2e5acece0a9d09ab0817113a19ca9ae68f761f29
Jaydev Khedkar <jaydevkhedkar257@gmail.com>
2026-09-14 00:29:11 +0530

    Time: 245 ms (52.63%), Space: 21.6 MB (72.99%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,25 +1,29 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        hashset = set()
-        res = []
-        def backtrack(r, c, curr):
-            if min(r, c) < 0 or r >= len(grid) or c >= len(grid[0]) or (r, c) in hashset:
-                return []
-            elif grid[r][c] == "1":
-                hashset.add((r,c))
-                curr.append((r,c))
-                backtrack(r + 1, c, curr)
-                backtrack(r - 1, c, curr)
-                backtrack(r, c + 1, curr)
-                backtrack(r, c - 1, curr)
-            return curr
-        for i in range(len(grid)):
-            for j in range(len(grid[0])):
-                if grid[i][j] == "0":
-                    continue
-                if (i, j) in hashset:
-                    continue
-                res.append(backtrack(i, j, []))
-        return len(res)
+        directions = [[1, 0], [-1, 0], [0, 1], [0, -1]]
+        ROWS, COLS = len(grid), len(grid[0])
+        islands = 0
 
-            
+        def bfs(r, c):
+            q = deque()
+            grid[r][c] = "0"
+            q.append((r, c))
+
+            while q:
+                row, col = q.popleft()
+                for dr, dc in directions:
+                    nr, nc = dr + row, dc + col
+                    if (nr < 0 or nc < 0 or nr >= ROWS or
+                        nc >= COLS or grid[nr][nc] == "0"
+                    ):
+                        continue
+                    q.append((nr, nc))
+                    grid[nr][nc] = "0"
+
+        for r in range(ROWS):
+            for c in range(COLS):
+                if grid[r][c] == "1":
+                    bfs(r, c)
+                    islands += 1
+
+        return islands
