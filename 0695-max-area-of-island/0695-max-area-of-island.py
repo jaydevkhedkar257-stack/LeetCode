a18ebaabb1d2839782efd8ab62dc7795db1224d8
Jaydev Khedkar <jaydevkhedkar257@gmail.com>
2026-09-14 12:34:08 +0530

    Time: 15 ms (84.82%), Space: 20.6 MB (53.28%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.py
+++ 0695-max-area-of-island/0695-max-area-of-island.py
@@ -1,15 +1,14 @@
 class Solution:
     def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
-        hashset = set()
         ROW = len(grid)
         COL = len(grid[0])
         max_island = 0
 
         def backtrack(r, c):
-            if min(r, c) < 0 or r >= ROW or c >= COL or (r, c) in hashset or grid[r][c] == 0:
+            if min(r, c) < 0 or r >= ROW or c >= COL or grid[r][c] == 0:
                 return 0
             if grid[r][c] == 1:
-                hashset.add((r, c))
+                grid[r][c] = 0
                 count = (backtrack(r, c + 1) + 
                          backtrack(r, c - 1) + 
                          backtrack(r + 1, c) + 
@@ -19,7 +18,7 @@ class Solution:
         
         for i in range(ROW):
             for j in range(COL):
-                if (i, j) in hashset or grid[i][j] == 0:
+                if grid[i][j] == 0:
                     continue
                 temp = backtrack(i , j)
                 max_island = max(max_island, temp)
