1b395cd78fe16b8b3a262ce3f4acfd44fef2ab2a
Jaydev Khedkar <jaydevkhedkar257@gmail.com>
2026-09-14 12:31:53 +0530

    Time: 18 ms (65.08%), Space: 23.8 MB (15.27%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.py
+++ 0695-max-area-of-island/0695-max-area-of-island.py
@@ -5,22 +5,23 @@ class Solution:
         COL = len(grid[0])
         max_island = 0
 
-        def backtrack(r, c, curr):
+        def backtrack(r, c):
             if min(r, c) < 0 or r >= ROW or c >= COL or (r, c) in hashset or grid[r][c] == 0:
-                return []
+                return 0
             if grid[r][c] == 1:
                 hashset.add((r, c))
-                curr.append((r, c))
-                backtrack(r, c + 1, curr)
-                backtrack(r, c - 1, curr)
-                backtrack(r + 1, c, curr)
-                backtrack(r - 1, c, curr)
-            return curr
+                count = (backtrack(r, c + 1) + 
+                         backtrack(r, c - 1) + 
+                         backtrack(r + 1, c) + 
+                         backtrack(r - 1, c) + 1)
+
+            return count
         
         for i in range(ROW):
             for j in range(COL):
                 if (i, j) in hashset or grid[i][j] == 0:
                     continue
-                temp = backtrack(i , j, [])
-                max_island = max(max_island, len(temp))
+                temp = backtrack(i , j)
+                max_island = max(max_island, temp)
+
         return max_island
