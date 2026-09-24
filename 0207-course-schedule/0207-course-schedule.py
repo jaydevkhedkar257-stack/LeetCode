68a1405b6b4b99bcf6adb12e31943251b70b59e8
Jaydev Khedkar <jaydevkhedkar257@gmail.com>
2026-09-24 23:43:40 +0530

    Time: 4 ms (64.04%), Space: 21.2 MB (25.96%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.py
+++ 0207-course-schedule/0207-course-schedule.py
@@ -1,26 +1,29 @@
 class Solution:
-    def canFinish(self, numCourses: int, prerequisites: list[list[int]]) -> bool:
-        visitSet = set()
-        preMap = {i:[] for i in range(numCourses)}
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        # Map each course to its prerequisites
+        preMap = {i: [] for i in range(numCourses)}
         for crs, pre in prerequisites:
             preMap[crs].append(pre)
-        
+
+        # Store all courses along the current DFS path
+        visiting = set()
 
         def dfs(crs):
-            if crs in visitSet:
+            if crs in visiting:
+                # Cycle detected
                 return False
             if preMap[crs] == []:
                 return True
 
-            visitSet.add(crs)
-            for i in preMap[crs]:
-                if not dfs(i): return False
-            visitSet.remove(crs)
-
+            visiting.add(crs)
+            for pre in preMap[crs]:
+                if not dfs(pre):
+                    return False
+            visiting.remove(crs)
             preMap[crs] = []
             return True
-        
-        for i in prerequisites:
-            if not dfs(i[0]):
+
+        for c in range(numCourses):
+            if not dfs(c):
                 return False
         return True
